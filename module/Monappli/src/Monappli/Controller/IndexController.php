33e880355d04069f46c152ca85de2d53b4623aeb
Orimbato_dev <freddyhat122@gmail.com>
2016-10-16 20:12:50 +0200

    ajout fonction export

--- module/Monappli/src/Monappli/Controller/IndexController.php
+++ module/Monappli/src/Monappli/Controller/IndexController.php
@@ -85,5 +85,66 @@ class IndexController extends AbstractActionController
                   ->setTerminal(true);
 		return $viewModel;
 	}
+	
+	public function ExportAction(){
+		
+		$em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+		$data = $em->getRepository('Monappli\Entity\dossier')->findAll();
+		
+		
+		
+		$fichier       = 'export.xls';
+		$inputFileName = EXCEL_PATH.'/'.$fichier;
+		$objPHPExcel = new \PHPExcel();
+		if (!file_exists($inputFileName)) {
+		  exit("Please run 14excel7.php first.\n");
+		}
+		
+		$n = 1;
+		
+		$objet       = \PHPExcel_IOFactory::createReader('Excel5');
+		$objPHPExcel = $objet->load($inputFileName);
+		$objPHPExcel->createSheet();
+		$sheet =  $objPHPExcel->setActiveSheetIndex($n);
+		$titre = "export_dossier";
+		$sheet->setTitle($titre);
+		$objPHPExcel->getActiveSheet($n)->setCellValue('A1','Dossier'); 
+		$objPHPExcel->getActiveSheet($n)->mergeCells('A1:C1');
+		
+		$objPHPExcel->getActiveSheet($n)->setCellValue('A2','Code client');
+		$objPHPExcel->getActiveSheet($n)->setCellValue('B2','Nom client');
+		$objPHPExcel->getActiveSheet($n)->setCellValue('C2','Date de numerisation');
+		$objPHPExcel->getActiveSheet($n)->getColumnDimension('A')->setAutoSize(true);
+		$objPHPExcel->getActiveSheet($n)->getColumnDimension('B')->setAutoSize(true);
+		$objPHPExcel->getActiveSheet($n)->getColumnDimension('C')->setAutoSize(true);
+		$objPHPExcel->getActiveSheet($n)->freezePane('A3');
+		
+		$ligne = $n+2;
+		foreach($data as $key=>$row)
+		{
+			$objPHPExcel->getActiveSheet($n)->setCellValue('A'.$ligne,$row->getNumeroClient());
+			$objPHPExcel->getActiveSheet($n)->setCellValue('B'.$ligne,$row->getNomClient());
+			$objPHPExcel->getActiveSheet($n)->setCellValue('C'.$ligne,$row->getDateNumerisation()->format('d-m-Y'));
+			// echo '<pre>';
+				// print_r($row->getNumeroClient());
+				// print_r($row->getNomClient());
+				// print_r($row->getDateNumerisation()->format('d-m-Y'));
+			// echo '</pre>';
+			
+			$ligne++;
+		}
+		
+		
+		
+		$objPHPExcel->removeSheetByIndex(0);
+		$objPHPExcel->setActiveSheetIndex(0);
+		$file = 'commande.xls';
+		$objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5'); 
+		header('Content-Type: application/vnd.ms-excel');
+		header('Content-Disposition: attachment;filename="'.$file.'"');
+		$objWriter->save(EXCEL_PATH.'/'.$file);
+		readfile(EXCEL_PATH.'/'.$file);
+		exit;
+	}
 }
 
